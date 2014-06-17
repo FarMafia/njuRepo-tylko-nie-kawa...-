39e6d2442f1153ca230b1511368323239a5d78fb
staddaleko <staddaleko@gmail.com>
2014-06-17 16:31:50 +0200

    cokolwiek - tylko wtedy laczy...!!!

---
+++ TestMock/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Rhino;
+using RozliczeniePracownikow;
+using NUnit;
+
+namespace TestMock
+{
+    [NUnit.Framework.TestFixture]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        [NUnit.Framework.Test]
+        public void testKosztPrzejazduSamochod()
+        {
+            //var mocks = new Rhino.Mocks.MockRepository();
+
+            //var rozliczeniaMock = mocks.StrictMock<RozliczeniePracownikow.Rozliczenia>();//tworzymy test w rozliczeniu pracownika //przeciazamy klase Rozliczenia w celu testowania danych
+            //Rhino.Mocks.Expect.Call(rozliczeniaMock.kosztPrzejazduSamochod(1000, 10F, 5F, 50F)).Return(100.00); //sfałszownaie wyniku metody dla podanych parametrów
+            //mocks.ReplayAll(); //wdrożenie mocków
+
+            //Console.WriteLine(rozliczeniaMock.kosztPrzejazduSamochod(1000, 10F, 5F, 50F));//przy jakiejkolwiek zmianie ten mock nie zadziała. Powyzszy kod bysmy dali podczas testowania w projekcie testujacym/
+            //Console.ReadLine();
+            //powyższe jest dla RHINO
+
+            //odtąd jest NUNIT
+            var mocks = new Rhino.Mocks.MockRepository();
+            var rozliczeniaMock = mocks.StrictMock<RozliczeniePracownikow.Rozliczenia>();//tworzymy test w rozliczeniu pracownika //przeciazamy klase Rozliczenia w celu testowania danych
+            Rhino.Mocks.Expect.Call(rozliczeniaMock.kosztPrzejazduSamochod(1000, 10F, 5F, 50F)).Return(100.00); //sfałszownaie wyniku metody dla podanych parametrów
+            mocks.ReplayAll(); //wdrożenie mocków
+
+            NUnit.Framework.Assert.AreEqual(100, rozliczeniaMock.kosztPrzejazduSamochod(1000, 10F, 5F, 50F));
+           
+        }
+    }
+}
+
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using RozliczeniePracownikow;
+using NUnit;
+
+
+namespace UnitTestProject1
+{
+    //[Nunit.Framework.TestFixture]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void kosztPrzejazduSamochod()
+        {
+            var kontekst = new RozliczeniePracownikow.Rozliczenia();
+            var aktualna = kontekst.kosztPrzejazduSamochod(900, 5.5F, 5.3F, 54.0F);
+            var oczekiwana = 316.35;
+
+            Assert.AreEqual<double>(oczekiwana, aktualna);
+        }
+
+        [TestMethod]
+        public void chorobowe()
+        {
+            var kontekst = new RozliczeniePracownikow.Rozliczenia();
+            var aktualna = kontekst.chorobowe(10, 3650.0F);
+            var oczekiwana = 973.30M;
+
+            Assert.AreEqual<decimal>(oczekiwana, aktualna);
+        }
+
+    }
+}
